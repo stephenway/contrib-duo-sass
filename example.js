4d3b40f6cd879c3fd470d467c34c6c9ed964711e
Stephen Way <stephen@agencyrevolution.com>
2014-08-28 11:45:01 -0700

    Fixed typo

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ var sass = require('./');
 co(function *(){
   var duo = Duo(__dirname + '/test/fixtures/simple');
   duo.entry('index.scss');
-  duo.use(stylus());
+  duo.use(sass());
   var css = yield duo.run();
   console.log(css)
 })();
